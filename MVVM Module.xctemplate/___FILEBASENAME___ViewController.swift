b41650e40103d72c78066fccf60b779b390e7947
Ron Daryl Magno <rmagno@yondu.com>
2018-10-17 16:24:16 +0800

    Initial commit

---
+++ MVVM Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    var viewModel: ___VARIABLE_productName___ViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MVVM Module.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,11 @@
+//___FILEHEADER___
+
+import Foundation
+
+struct ___FILEBASENAMEASIDENTIFIER___ {
+    private let repository: ___VARIABLE_productName___Repository
+
+    init(repository: ___VARIABLE_productName___Repository) {
+        self.repository = repository
+    }
+}
